6f5914f6bc0fdd139fa51c267ef090af31d3f559
Maxim Shipko <sladethe@gmail.com>
2023-01-19 21:07:11 +0400

    Speed up UUID check

--- vstring/uuid.go
+++ vstring/uuid.go
@@ -1,17 +1,19 @@
 package vstring
 
 import (
-	"regexp"
-
 	"github.com/SladeThe/yav"
 )
 
+const (
+	uuidLength = 36
+)
+
 var (
-	uuidRegex = regexp.MustCompile("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$")
+	uuidPartLengths = [5]int{8, 4, 4, 4, 12}
 )
 
 func UUID(name string, value string) (stop bool, err error) {
-	if !uuidRegex.MatchString(value) {
+	if !isUUID(value) {
 		return true, yav.Error{
 			CheckName: yav.CheckNameUUID,
 			ValueName: name,
@@ -20,4 +22,33 @@ func UUID(name string, value string) (stop bool, err error) {
 	}
 
 	return false, nil
+}
+
+func isUUID(value string) bool {
+	if len(value) != uuidLength {
+		return false
+	}
+
+	partIndex := 0
+	partLength := uuidPartLengths[0]
+
+	for _, r := range []byte(value) {
+		if partLength > 0 {
+			if (r < '0' || r > '9') && (r < 'a' || r > 'f') {
+				return false
+			}
+
+			partLength--
+			continue
+		}
+
+		if r != '-' {
+			return false
+		}
+
+		partIndex++
+		partLength = uuidPartLengths[partIndex]
+	}
+
+	return true
 }
