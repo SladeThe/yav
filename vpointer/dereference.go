d3d6670f3cc25adfcf50cb69fae6aaca9975bc35
Maxim Shipko <sladethe@gmail.com>
2023-10-06 03:43:39 +0400

    Update vpointer.Dereference

--- vpointer/dereference.go
+++ vpointer/dereference.go
@@ -8,6 +8,15 @@ func Dereference[T any](validateFuncs ...yav.ValidateFunc[T]) yav.ValidateFunc[*
 	return func(name string, value *T) (stop bool, err error) {
 		var yavErrs yav.Errors
 
+		if value == nil {
+			yavErrs.Append(yav.Error{
+				CheckName: yav.CheckNameRequired,
+				ValueName: name,
+			})
+
+			return true, yavErrs.AsError()
+		}
+
 		for _, validateFunc := range validateFuncs {
 			stop, err = validateFunc(name, *value)
 			yavErrs.Append(err)
